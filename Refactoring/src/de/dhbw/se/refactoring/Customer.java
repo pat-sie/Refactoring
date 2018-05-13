44efff425bbc6dfbba1e78220fb71cd43989fe75
Patrick <pat-sie@web.de>
2018-05-13 23:34:04 +0200

    comments: created methods and constants to make the code self-explanatory

--- Refactoring/src/de/dhbw/se/refactoring/Customer.java
+++ Refactoring/src/de/dhbw/se/refactoring/Customer.java
@@ -26,24 +26,34 @@ class Customer {
 		result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
 		while (enum_rentals.hasMoreElements()) {
-			double thisAmount = 0;
-			Rental each = (Rental) enum_rentals.nextElement();
-			// determine amounts for each line
-			thisAmount = amountFor(each);
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPoints++;
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			Rental rental = (Rental) enum_rentals.nextElement();
+			double amountForRental = amountFor(rental);
+			frequentRenterPoints = addFrequentRenterPoints(frequentRenterPoints);
+			if (isTwoDayNewReleaseRental(rental))
+				frequentRenterPoints = addFrequentRenterPoints(frequentRenterPoints);
+			result += showFiguresForRental(result, rental, amountForRental);
+			totalAmount += amountForRental;
 		}
-		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		result += addFooterLines(totalAmount, frequentRenterPoints, result);
 		return result;
+	}
+
+	private String addFooterLines(double totalAmount, int frequentRenterPoints, String result) {
+		return "Amount owed is " + String.valueOf(totalAmount) + "\n" + "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+	}
+
+	private String showFiguresForRental(String result, Rental rental, double amountForRental) {
+		return "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t"
+				+ String.valueOf(amountForRental) + "\n";
+	}
+
+	private boolean isTwoDayNewReleaseRental(Rental rental) {
+		return (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1;
+	}
+
+	private int addFrequentRenterPoints(int frequentRenterPoints) {
+		frequentRenterPoints++;
+		return frequentRenterPoints;
 	}
 
 	public double amountFor(Rental rental) {
